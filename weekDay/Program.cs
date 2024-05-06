05b069af22f0d42c42541b244781c3ea9c3f6140
Hemze <hmztarverdiyev@gmail.com>
2024-05-07 01:03:51 +0400

    move

--- weekDay/Program.cs
+++ weekDay/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1-7 arasında bir ədəd daxil ed:");
+            int gun = int.Parse(Console.ReadLine());
+
+            switch (gun)
+            {
+                case 1:
+                    Console.WriteLine("Bazar ertəsi");
+                    break;
+                case 2:
+                    Console.WriteLine("Çərşənbə axşamı");
+                    break;
+                case 3:
+                    Console.WriteLine("Çərşənbə");
+                    break;
+                case 4:
+                    Console.WriteLine("Cümə axşamı");
+                    break;
+                case 5:
+                    Console.WriteLine("Cümə");
+                    break;
+                case 6:
+                    Console.WriteLine("Şənbə");
+                    break;
+                case 7:
+                    Console.WriteLine("Bazar");
+                    break;
+                default:
+                    Console.WriteLine("Düzgün ədəd daxil ed.");
+                    break;
+            }
         }
     }
 }
+        
